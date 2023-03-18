1e1f3b37d797ab21168442b0f722b3dc83086f69
Joel Okorie <joelokorie1@gmail.com>
2023-03-18 07:57:25 +0000

    101 quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <unistd>
+#include <unistd.h>
 /**
  *main - A program that prints a line to the standard error
  *Return: 1 (success)
@@ -7,6 +7,6 @@
 
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }

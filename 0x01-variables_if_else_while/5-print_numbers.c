43f22323cc643cd345f50291bc3fdea8cdc520b3
Joel Okorie <joelokorie1@gmail.com>
2023-03-20 10:47:13 +0000

    Print numbers in base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main -  print a number in decimal base
+
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		printf("%d", n);
+	}
+	printf("\n");
+
+	return (0);
+}

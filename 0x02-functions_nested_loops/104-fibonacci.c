22631a1979b81eb142ef2ceac06332e164b8cf65
Joel Okorie <joelokorie1@gmail.com>
2023-03-22 11:52:56 +0000

    104 fibonacci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -18,6 +18,7 @@ int main(void)
 	{
 		sum = fib1 + fib2;
 		printf("%lu, ", sum);
+		
 		fib1 = fib2;
 		fib2 = sum;
 																		}
@@ -41,10 +42,11 @@ int main(void)
 
 			if (count != 98)
 				printf(", ");
-				fib1_half1 = fib2_half1;
-				fib1_half2 = fib2_half2;
-				fib2_half1 = half1;
-				fib2_half2 = half2;
+				
+			fib1_half1 = fib2_half1;
+			fib1_half2 = fib2_half2;
+			fib2_half1 = half1;
+			fib2_half2 = half2;
 		}
 
 		printf("\n");

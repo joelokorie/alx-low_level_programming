5dc0c95a9c1946f4c89558013a6fad17a56682d3
Joel Okorie <joelokorie1@gmail.com>
2023-03-31 22:14:37 +0000

    Concatenate a certain number of strings

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strncat - concatenates two strings
+ * @dest: input value
+ * @src: input value
+ * @n: the number of characters to be concatenated
+ * Return: (dest)
+ *
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int destlength = 0;
+	int srclength = 0;
+	int i;
+
+	for (i = 0; dest[i] != '\0'; i++)
+		destlength++;
+
+	for (i = 0; src[i] != '\0'; i++)
+		srclength++;
+
+	for (i = 0; i < n; i++)
+		dest[destlength + i] = src[i];
+
+	return (dest);
+}

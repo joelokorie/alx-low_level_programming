a4261acd78bd5e9ad0e4f5f2a419f70cc81fcdfe
Joel Okorie <joelokorie1@gmail.com>
2023-03-31 22:47:49 +0000

    Concatenating a certain number of characters

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,18 +10,20 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int destlength = 0;
-	int srclength = 0;
-	int i;
-
-	for (i = 0; dest[i] != '\0'; i++)
-		destlength++;
-
-	for (i = 0; src[i] != '\0'; i++)
-		srclength++;
-
-	for (i = 0; i < n; i++)
-		dest[destlength + i] = src[i];
+	int j;
+	j = 0;
+	
+	while (j < n && src[j] != '\0')
+	{
+		dest[j] = src[j];
+		j++;
+	}
+	
+	while (j < n)
+	{
+		dest[j] = '\0';
+		j++;
+	}
 
 	return (dest);
 }

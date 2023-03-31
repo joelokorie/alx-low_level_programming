7fc14b177c008a4779ba27433229ec9ffb31764a
Joel Okorie <joelokorie1@gmail.com>
2023-03-31 22:49:39 +0000

    concatenations

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,13 +12,13 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int j;
 	j = 0;
-	
+
 	while (j < n && src[j] != '\0')
 	{
 		dest[j] = src[j];
 		j++;
 	}
-	
+
 	while (j < n)
 	{
 		dest[j] = '\0';

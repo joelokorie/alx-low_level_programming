8d09e9d49a8b215fdf45b012990a4d2f59c2eefb
Joel Okorie <joelokorie1@gmail.com>
2023-04-01 01:05:23 +0000

    copy strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,18 +12,11 @@ char *_strncat(char *dest, char *src, int n)
 {
 
 	int destlength = 0;
-	/**
-	 * int srclength = 0;
-	 */
 	int i;
 
 	for (i = 0; dest[i] != '\0'; i++)
 		destlength++;
 
-	/**
-	 * for (i = 0; src[i] != '\0'; i++)
-	 *	srclength++;
-	 */
 	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[destlength + i] = src[i];
 

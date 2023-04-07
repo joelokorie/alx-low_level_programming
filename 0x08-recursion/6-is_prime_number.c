f08969ed5e1b17f3b34300c413e190be0e60e6b5
Joel Okorie <joelokorie1@gmail.com>
2023-04-07 02:37:53 +0000

    A function that checks for prime numbers

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+
+
+int check_prime(int n, int x);
+
+/**
+  * is_prime_number - checks if a number is prime
+  * @n: the number to be checked
+  *
+  * Return: integer value
+  */
+int is_prime_number(int n)
+{
+	return (check_prime(n, 1));
+}
+
+/**
+  * check_prime - Checks if number is prime
+  * @n: the number to be checked
+  * @x: the number of times iteration is to be done
+  *
+  * Return: 1 for prime or 0 composite
+  */
+int check_prime(int n, int x)
+{
+	if (n <= 1)
+		return (0);
+
+	if (n % x == 0 && x > 1)
+		return (0);
+
+	if ((n / x) < x)
+		return (1);
+
+	return (check_prime(n, x + 1));
+}

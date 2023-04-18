018ac7993bd93a6e42e8e4ba8ad8939344f04ced
Joel Okorie <joelokorie1@gmail.com>
2023-04-18 23:53:20 +0000

    a function about struct

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,17 +1,19 @@
-#include <stdio.h>
+#include <stdlib.h>
 #include "dog.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
+  * free_dog - ...
+  * @d: ...
+  *
+  * Return: ...
+  */
+void free_dog(dog_t *d)
 {
-    dog_t *my_dog;
-
-    my_dog = new_dog("Poppy", 3.5, "Bob");
-    printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog->name, my_dog->age);
-    free_dog(my_dog);
-    return (0);
+	if (d != NULL)
+	{
+		free(d->owner);
+		free(d->name);
+		free(d);
+	}
 }
+
